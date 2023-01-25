a2e33ed58f57f75d1eb830ee028564b5a01262e5
JoaoVitorSD <joaovitorsantanadepollo@gmail.com>
2023-01-25 06:47:20 -0300

    build: switch to maven

---
+++ src/main/java/entities/Atividade.java
@@ -0,0 +1,13 @@
+package entities;
+
+public class Atividade {
+    private String titulo;
+
+    private String descricao;
+
+    private Integer cargaHoraria;
+
+    public Long calcXp(){
+        return 0L;
+    }
+}
---
+++ src/main/java/entities/Dev.java
@@ -0,0 +1,23 @@
+package entities;
+
+import java.util.List;
+
+public class Dev {
+    private String nome;
+    private List<Mentoria> mentorias;
+
+    private List<Atividade> atividades;
+
+    public void inscreverBootcamp(){
+
+    }
+    public void progredir(){
+
+    }
+    public void calcTotalXp(){
+
+    }
+    public void exibirCursos(){
+
+    }
+}
